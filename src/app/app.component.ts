ec89840ff9ab40687487d21349fec90611cabfe0
borgesgh <pauloborgeszambon@gmail.com>
2025-06-06 23:09:36 -0300

    acrsebt imgs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {BuscarComponent} from './components/buscar/buscar.component';
+import {MainComponent} from './components/main/main.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BuscarComponent, MainComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
